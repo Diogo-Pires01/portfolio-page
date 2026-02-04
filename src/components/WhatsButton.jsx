528c0df5b16b61102e2ac4ad289f78b9ba83cb04
Diogo Jose Guimaraes Pires <diogo.pires@dataprev.gov.br>
2026-02-04 15:20:54 -0300

    ajuste wpp btn

--- src/components/WhatsButton.jsx
+++ src/components/WhatsButton.jsx
@@ -2,7 +2,7 @@ import { MessageCircle } from "lucide-react";
 
 const WhatsButton = () => {
   return (
-    <div className="fixed bottom-10 right-10 bg-green-500 z-50 rounded-full p-3">
+    <div className="fixed bottom-5 right-5 md:bottom-10 md:right-10 bg-green-500 z-50 rounded-full p-3">
       <a href="https://wa.me/5521996326905?text=Olá,%20vim%20pelo%20seu%20portfólio" target="_blank">
         <MessageCircle size={32} className="text-white" />
       </a>
