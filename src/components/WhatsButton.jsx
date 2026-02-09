ca8f9f272f6e16d9efa92d74316df198fd929af2
Diogo Jose Guimaraes Pires <diogo.pires@dataprev.gov.br>
2026-02-09 16:50:27 -0300

    ajustando msg

--- src/components/WhatsButton.jsx
+++ src/components/WhatsButton.jsx
@@ -4,7 +4,7 @@ const WhatsButton = () => {
   return (
     <div className="fixed bottom-5 right-5 md:bottom-10 md:right-10 bg-green-500 z-50 rounded-full p-3">
       <a
-        href="https://wa.me/5521996326905?text=Olá,%20vim%20pelo%20seu%20portfólio!"
+        href="https://wa.me/5521996326905?text=Olá,%20gostaria%20de%20saber%20mais!"
         target="_blank"
       >
         <MessageCircle size={32} className="text-white" />
